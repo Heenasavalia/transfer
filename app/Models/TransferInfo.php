6b583fa4b2daa557b855de1fe812ea83296f0a64
heenasavalia <heenasavaliya2704@gmail.com>
2021-08-04 16:20:29 +0530

    create new api for add entry in image and voyges table

--- app/Models/TransferInfo.php
+++ app/Models/TransferInfo.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Helpers;
 
 class TransferInfo extends Model
 {
@@ -11,6 +12,16 @@ class TransferInfo extends Model
 
     protected $table = "transfer_info";
     protected $fillable = [
-        'voyage_id','type','file_name','is_delete','created_at','updated_at'
+        'voyage_id','type','file_name','is_delete','created_at','updated_at','ref_id'
     ];
+
+    public function getFileNameAttribute($value)
+    {
+        // dd($value);
+        if($value){
+            return Helpers::manageUploadFileLink('file_data',$value);
+        }else{
+            return asset('images/default.png');
+        }
+    }
 }
--- app/Models/Voyage.php
+++ app/Models/Voyage.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Voyage extends Model
 {
@@ -11,7 +12,31 @@ class Voyage extends Model
 
     protected $table = "voyages";
     protected $fillable = [
-        'user_id','receiver_id','type','image_ids','video_ids','contact_ids','audio_ids',
-        'document_id','location_id','is_read','is_delete','auto_delete','is_download','file_id','expired_at'
+        'user_id', 'receiver_id', 'type', 'image_ids', 'video_ids', 'contact_ids', 'audio_ids',
+        'document_id', 'location_id', 'is_read', 'is_delete', 'auto_delete', 'is_download', 'file_id', 'expired_at', 'description',
+        'receiver_email'
     ];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return \Carbon\Carbon::parse($value)->format('d-M-Y H:i A');
+    }
+    
+    public function transfer()
+    {
+        return $this->belongsTo('App\Models\TransferInfo');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+    public function receiver()
+    {
+        return $this->belongsTo('App\Models\User', 'receiver_id', 'id');
+    }
+    public function sender()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id', 'id');
+    }
 }
